ace9ae9b99064f488a815fbcadf6c8363bee0e76
Hazko3a <james.owens0508@gmail.com>
2026-02-06 12:24:47 +0000

    flywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/java/mechanisms/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/java/mechanisms/DriveTrain.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.java.mechanisms;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 
 
 @TeleOp
@@ -16,9 +16,16 @@ public class DriveTrain extends OpMode {
     public DcMotor  rightDrive  = null;
     public DcMotor collectionWheel = null;
 
+    public DcMotor flyWheel = null;
+
     //boolean for the collection wheel
     private boolean collectionWheelOn = false;
     private boolean bPressed = false;
+
+
+    private boolean flyWheelOn = false;
+    private boolean aPressed = false;
+
 
     @Override
     public void init() {
@@ -29,6 +36,9 @@ public class DriveTrain extends OpMode {
         // for collection of balls on the robot
         collectionWheel = hardwareMap.get(DcMotor.class, "Hex Motor 2");
 
+        //for the flywheel
+        flyWheel = hardwareMap.get(DcMotor.class, "Flywheel Motor 3");
+
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left and right sticks forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
@@ -36,8 +46,9 @@ public class DriveTrain extends OpMode {
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        collectionWheel.setDirection(DcMotor.Direction.REVERSE
-        01-);
+        collectionWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        flyWheel.setDirection(DcMotor.Direction.REVERSE);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press START.");    //
@@ -66,6 +77,18 @@ public class DriveTrain extends OpMode {
             collectionWheel.setPower(1.0);
         } else {
             collectionWheel.setPower(0.0);
+        }
+
+        //flywheel code
+        if (gamepad1.a && !aPressed) {
+            flyWheelOn = !flyWheelOn;
+        }
+        aPressed = gamepad1.a;
+
+        if (flyWheelOn) {
+            flyWheel.setPower(2.0);
+        } else {
+            flyWheel.setPower(0.0);
         }
     }
 }
