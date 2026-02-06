a817d1a7ea9c3b5fd4c13860cd10d26448a737a6
Hazko3a <james.owens0508@gmail.com>
2026-02-06 14:04:42 +0000

    Update DriveTrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/java/mechanisms/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/java/mechanisms/DriveTrain.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.java.mechanisms;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 
 
 @TeleOp
